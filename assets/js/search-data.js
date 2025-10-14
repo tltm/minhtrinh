31f3d811840e51621f76b4a132d3ef157334203a
tltm <zcemltm@ucl.ac.uk>
2025-10-14 16:36:19 +0000

    Deploying to gh-pages from @ tltm/minhtrinh@99688a50528bc9afaf93f78df406fa28a5d3b862 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,8 +398,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/minhtrinh/books/the_godfather/";
-            },},{id: "news-a-paper-got-accepted-to-neurips-2025-workshop-in-interpreting-cognition",
-          title: 'A paper got accepted to NeurIPS 2025 Workshop in Interpreting Cognition.',
+            },},{id: "news-a-paper-got-accepted-to-neurips-2025-workshop-in-interpreting-cognition-submission-version",
+          title: 'A paper got accepted to NeurIPS 2025 Workshop in Interpreting Cognition. (submission version)...',
           description: "",
           section: "News",},{
         id: 'social-email',
